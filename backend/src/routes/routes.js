8c81c672784432e462a95955abdf7c58f4a57228
rahul-rvarma <rahul.rvarmaa@gmail.com>
2025-11-26 10:40:53 +0530

    new commit

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/", create_note);
 router.get("/", get_all_notes);
 router.get("/:id", get_specific_note);
-router.patch("/:id", update_note);
+router.put("/:id", update_note);
 router.delete("/:id", delete_note);
 
 export default router;
