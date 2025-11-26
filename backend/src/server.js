160e4d7c6570e9c4e1d05ce4678c5774dfd42ce3
rahul-rvarma <rahul.rvarmaa@gmail.com>
2025-11-26 09:49:30 +0530

    completed the app

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,25 @@ import dotenv from "dotenv";
 import routes from './routes/routes.js';
 import connectDB from './config/db.js';
 import rateLimirer from './middlewares/rateLimitter.js';
+import path from 'path';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 //middleware to use routes
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json());
 
@@ -20,11 +32,23 @@ app.use(express.json());
 
 app.use(rateLimirer)
 
+
 app.use("/api/notes", routes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  });
+
+
+}
+
+
 connectDB().then(() => {
-  app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 }).catch((error) => {
   console.error("Failed to connect to the database:", error);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { Routes } from "react-router";
-import {Homepage} from "./pages/homepage.jsx";
-import {Createpage} from "./pages/createpage.jsx";
-import {Notedetailpage} from "./pages/notedetailpage.jsx";
-import { Route } from "react-router";
+import { Route, Routes } from "react-router";
+
+import HomePage from "./pages/homepage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
-    return <div>
-        <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/create" element={<Createpage />} />
-            <Route path="/note/:id" element={<Notedetailpage />} />
-        </Routes>
+  return (
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
     </div>
-}
+  );
+};
 export default App;
